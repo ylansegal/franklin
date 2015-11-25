2bfe406987c98ddf8911aea4c1bcd9fc912af004
Ylan Segal <ylan@segal-family.com>
2015-11-25 11:30:19 -0800

    Add end-to-end integration spec, for sanity

--- lib/franklin.rb
+++ lib/franklin.rb
@@ -5,5 +5,14 @@ require "franklin/collate"
 require "franklin/console_report"
 
 module Franklin
+  def run(search_terms, out)
+    config = Config.load_from_file
+    individual_results = config.libraries.map { |library|
+      Franklin::Search.new(library).perform("Forward The Foundation")
+    }
+    collated_results = Franklin::Collate.new.perform(individual_results)
+    ConsoleReport.new(search_terms, collated_results).print_to_out(out)
+  end
 
+  module_function :run
 end
--- spec/lib/franklin_spec.rb
+++ spec/lib/franklin_spec.rb
@@ -4,4 +4,10 @@ describe Franklin do
   it "has a version number" do
     expect(Franklin::VERSION).not_to be nil
   end
+
+  describe ".run", vcr: true do
+    it "integrates end-to-end" do
+      expect { Franklin.run("Seveneves", StringIO.new) }.to_not raise_error
+    end
+  end
 end
