6cb9ae56cafd90babfd34c7620fa632a3a3eaabf
Ylan Segal <ylan@segal-family.com>
2015-11-25 11:17:56 -0800

    Add output decorator for Availability

---
+++ lib/franklin/availability_description.rb
@@ -0,0 +1,21 @@
+module Franklin
+  class AvailabilityDescription < SimpleDelegator
+    def to_s
+      "#{copies_information} @ #{library.name}"
+    end
+
+    private
+
+    def copies_information
+      available? ? "Available" : "#{copies_per_person} people/copy"
+    end
+
+    def available?
+      available_copies > 0
+    end
+
+    def copies_per_person
+      (wait_list_size.to_f / total_copies.to_f).round(1)
+    end
+  end
+end
---
+++ spec/lib/franklin/availability_description_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+module Franklin
+  describe AvailabilityDescription do
+    describe "#to_s" do
+      let(:result) { described_class.new(availability).to_s }
+      let(:library) { Library.fixture }
+
+      context "when available_copies > 0" do
+        let(:availability) { Availability.new(library, 10, 2, 0) }
+
+        it "describes it as available" do
+          expect(result).to be == "Available @ #{availability.library.name}"
+        end
+      end
+
+      context "when available_copies = 0" do
+        let(:availability) { Availability.new(library, 4, 0, 13) }
+
+        it "describes the people per copy" do
+          expect(result).to be == "3.3 people/copy @ #{availability.library.name}"
+        end
+      end
+    end
+  end
+end
