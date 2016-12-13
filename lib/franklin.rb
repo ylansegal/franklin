f2a5b389bfa5d0c11d7271db30645fcf90a5e41f
Ylan Segal <ylan@segal-family.com>
2016-12-13 15:45:25 -0800

    Use keyword arguments

--- lib/franklin.rb
+++ lib/franklin.rb
@@ -5,11 +5,11 @@ require "franklin/type_filter"
 require "franklin/console_report"
 
 module Franklin
-  def run(search_terms, opts)
-    config = Config.load_from_file(opts[:config_path])
+  def run(search_terms, config_path: nil, type: nil, out:)
+    config = Config.load_from_file(config_path)
     results = ThreadedSearch.new(config.libraries).perform(search_terms)
-    filtered_results = TypeFilter.new(opts[:type] || config.default_type).perform(results)
-    ConsoleReport.new(search_terms, filtered_results).print_to_out(opts[:out])
+    filtered_results = TypeFilter.new(type || config.default_type).perform(results)
+    ConsoleReport.new(search_terms, filtered_results).print_to_out(out)
   end
 
   module_function :run
